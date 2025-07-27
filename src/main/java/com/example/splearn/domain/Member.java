4dc4d0a39de1984c0efb5b5443782e718ec0928d
RATIONAL331 <rational331@gmail.com>
2025-07-27 14:37:14 +0900

    add Member activation and deactivation methods with unit tests

--- src/main/java/com/example/splearn/domain/Member.java
+++ src/main/java/com/example/splearn/domain/Member.java
@@ -1,8 +1,13 @@
 package com.example.splearn.domain;
 
 import lombok.Getter;
+import lombok.ToString;
+import org.springframework.util.Assert;
+
+import java.util.Objects;
 
 @Getter
+@ToString
 public class Member {
 	private String email;
 	private String password;
@@ -10,9 +15,24 @@ public class Member {
 	private MemberStatus status;
 
 	public Member(String email, String password, String nickname) {
-		this.email = email;
-		this.password = password;
-		this.nickname = nickname;
+		this.email = Objects.requireNonNull(email);;
+		this.password = Objects.requireNonNull(password);
+		this.nickname = Objects.requireNonNull(nickname);
 		this.status = MemberStatus.PENDING;
+	}
+
+	public void activate() {
+//		if (getStatus() != MemberStatus.PENDING) {
+//			throw new IllegalStateException("Member is not in PENDING status");
+//		}
+		Assert.state(MemberStatus.PENDING.equals(this.status), "Member is not in PENDING status");
+
+		this.status = MemberStatus.ACTIVE;
+	}
+
+	public void deactivate() {
+		Assert.state(MemberStatus.ACTIVE.equals(this.status), "Member is not in PENDING status");
+
+		this.status = MemberStatus.DEACTIVATED;
 	}
 }
--- src/test/java/com/example/splearn/domain/MemberTest.java
+++ src/test/java/com/example/splearn/domain/MemberTest.java
@@ -10,4 +10,54 @@ class MemberTest {
 		Member member = new Member("test@test.com", "password123", "testuser");
 		Assertions.assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
 	}
+
+	@Test
+	void constructorNullCheck() {
+		Assertions.assertThatThrownBy(() -> new Member(null, "password123", "testuser"))
+		          .isInstanceOf(NullPointerException.class);
+	}
+
+	@Test
+	void activateMember() {
+		Member member = new Member("test@test.com", "password123", "testuser");
+
+		member.activate();
+
+		Assertions.assertThat(member.getStatus()).isEqualTo(MemberStatus.ACTIVE);
+	}
+
+	@Test
+	void activateFail() {
+		Member member = new Member("test@test.com", "password123", "testuser");
+
+		member.activate();
+
+		Assertions.assertThatThrownBy(member::activate)
+		          .isInstanceOf(IllegalStateException.class);
+	}
+
+	@Test
+	void deactivateMember() {
+		Member member = new Member("test@test.com", "password123", "testuser");
+		member.activate();
+
+		member.deactivate();
+
+		Assertions.assertThat(member.getStatus()).isEqualTo(MemberStatus.DEACTIVATED);
+	}
+
+	@Test
+	void deactivateFail() {
+		Member member = new Member("test@test.com", "password123", "testuser");
+
+		Assertions.assertThatThrownBy(member::deactivate)
+		          .isInstanceOf(IllegalStateException.class);
+
+		member.activate();
+
+		member.deactivate();
+
+		Assertions.assertThatThrownBy(member::deactivate)
+		          .isInstanceOf(IllegalStateException.class);
+	}
 }
